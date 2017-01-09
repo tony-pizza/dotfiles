17b771839927158e5635bf8aacc3d46764adf9b6
Ian Pearce <ian@ianpearce.org>
2017-01-09 11:23:03 -0800

    Backup dots in linker script

--- link.sh
+++ link.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mv ~/.gitconfig ~/.gitconfig.backup
+mv ~/.tmux.conf ~/.tmux.conf.backup
+mv ~/.vimrc ~/.vimrc.backup
+
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
